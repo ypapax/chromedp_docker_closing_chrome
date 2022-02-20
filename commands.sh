eb823ce656af6a9f40b1494ba1d08a0759b011f3
Maxim Yefremov <maxYefr@gmail.com>
2022-02-20 14:19:41 +0300

    build linux

--- commands.sh
+++ commands.sh
@@ -11,6 +11,12 @@ runl(){
   go run main.go
 }
 
+buildLinux(){
+  bin=~/tmp/chromedb_linux
+  GOOS=linux GOARCH=amd64  go build -o $bin
+  ls -laht $bin
+}
+
 rund(){
   docker build  -t chromedp-alpine .
   docker container run -it --rm --security-opt seccomp=$(pwd)/chrome.json chromedp-alpine
