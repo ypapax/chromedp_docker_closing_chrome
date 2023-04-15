9b064eede23f89a2406daa3ec1cc552a76888cb0
Maxim Yefremov <maxYefr@gmail.com>
2023-04-15 07:11:57 +0300

    wip 2 not working

--- commands.sh
+++ commands.sh
@@ -11,6 +11,12 @@ loca(){
   go run main.go
 }
 
+runChrome(){
+#  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --kiosk
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -remote-debugging-port 9222
+#  chrome
+}
+
 check(){
   container=$(docker ps | grep chrome | awk '{  print $10 }')
   docker exec -ti $container /bin/bash
